37e730db4b1c09a413647bb8f27cdbd5cd95bf55
Bill Brown <bill.a.brown90@gmail.com>
2023-03-28 09:11:24 -0700

    updated changes

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,17 +6,21 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+// root.render(
+ReactDOM.render(
+  <Router>
     <App />
-  </React.StrictMode>
-);
+  </Router>,
+  // document.getElementByIdID("root")
+  );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
