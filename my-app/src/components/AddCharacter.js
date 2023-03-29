2c4426326e7080f5f888be9cf81ee42eac55d202
Bill Brown <bill.a.brown90@gmail.com>
2023-03-29 08:42:08 -0700

    updating form

--- my-app/src/components/AddCharacter.js
+++ my-app/src/components/AddCharacter.js
@@ -1,116 +1,52 @@
- import React, {useState} from 'react';
+import React from 'react'
+import { Button, Checkbox, Form } from 'semantic-ui-react'
 
 
 function AddCharacter ({character}) { 
-  const [form, setForm] = useState({});
-  const handleChange = (e) => {
-    let name = e.target.name;
-    let value = e.target.value;
-    setForm({
-      ...form, 
-      [name]: value, 
-    });
-  };
-  let handleSubmit = (e) => {
-    e.preventDefault();
-    fetch('http://localhost.3001/characters', {
-      method: 'POST',
-      headers: {
-        "Content-Type": "application/json",
-        },
-        body: JSON.stringify(form),
-      })
-      .then((res) => res.json())
-      .then((data) => AddCharacter(character));
-    };
-    console.log(character);
-    return ( 
-      <div className="new-character-form">
-      <h2>Add Character</h2>
-      <form onSubmit={handleSubmit}>
-      <input
-      onChange={handleChange}
-      type="text"
-      name="name"
-      placeholder="Name"
-      />
-      <input
-      onChange={handleChange}
-      type="img"
-      name="image"
-      placeholder="Image URL"
-      />
-      <input
-      onChange={handleChange}
-      type="text"
-      name="Special"
-          placeholder="Special Moves"
-          />
-          <button type="submit">Add Character</button>
-          </form>
-          </div>
-          );
-        } 
 
+  return (
+    <Form>
+      <Form.Field>
+        <label>New Player Name</label>
+        <input placeholder='Enter Name Here' />
+      </Form.Field>
+      <Form.Field>
+        <label>Special Moves</label>
+        <input placeholder='Enter Any Special Moves' />
+      </Form.Field>
+      <Form.Field>
+        <label>Image</label>
+        <input placeholder='Enter Image URL' />
+      </Form.Field>
+      <Form.Field>
+        <label>Series</label>
+        <input placeholder='Enter Game Series' />
+      </Form.Field>
+      <Button type='submit'>Submit</Button>
+    </Form>
+  )
 
+ 
+}
 export default AddCharacter;
-        /* const baseURL = "http://localhost:3001/characters"
-        
-        
-        
-        function NewCharacterForm({addCharacter}) {
-          const [name, setName] = useState("");
-          const [image, setImage] = useState("");
-          const [specialMoves, setSpecialMoves] = useState("");
-          
-          function handleSubmit(e) {
-            e.preventDefault();
-            fetch(baseURL, {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                name,
-                image,
-          specialMoves
-        }),
-      })
-      .then((r) => r.json())
-      .then((newCharacters) => addCharacter(newCharacters));
-      
-      setName("");
-      setImage("");
-      setSpecialMoves("");
-    }
-    
-    return (
-      <form className="new-character-form" onSubmit={handleSubmit} >
-      <input
-      placeholder="Name"
-      value={name}
-      onChange={(e) => setName(e.target.value)}
-      />
-      
-      <input 
-      placeholder="Image"
-      value={image}
-      onChange={(e) => setImage(e.target.value)}
-      />
-      
-      <input 
-      placeholder="Special Moves"
-      rows={10}
-      value={specialMoves}
-      onChange={(e) => setSpecialMoves(e.target.value)}
-      />
-      
-      <input  
-      type="submit"
-      value="Add Character"
-      />
-      </form>
-      );
-    }
-    */
- //export default AddCharacter;
+
+  // {
+  //   "alsoAppearsIn": [
+  //     "SSB",
+  //     "Melee",
+  //     "Brawl",
+  //     "SSB4"
+  //   ],
+  //   "availability": "Starter",
+  //   "images":{
+  //     "icon":"https://res.cloudinary.com/dfen7mkm8/image/upload/v1594501674/Ultimate%20Characters/Icons/120px-MarioHeadSSBUWebsite_hkzzpq.png",
+  //     "portrait":"https://res.cloudinary.com/dfen7mkm8/image/upload/v1594501176/Ultimate%20Characters/Portraits/350_emjsmz.png"
+  //   },
+  //   "name":"Mario",
+  //   "order":"1",
+  //   "special": ["Fireball","Cape","Super Jump Punch","F.L.U.D.D."],
+  //   "tier": "A",
+  //   "series":{
+  //     "icon":"https://res.cloudinary.com/dfen7mkm8/image/upload/v1594501902/Series/mario_ncgezx.svg",
+  //     "name":"Mario"
+  //   }
