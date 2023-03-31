7fa1983ef100daa84dd3ed04f21b6a01f244ab48
Holden Gerner <hgerner19@gmail.com>
2023-03-31 11:49:09 -0600

    updated holden

--- my-app/src/components/Characters.js
+++ my-app/src/components/Characters.js
@@ -1,11 +1,34 @@
 
-import React from 'react';
+import React , {useState,useEffect}  from 'react';
 import CharacterCard from "./CharacterCard";
 import { Container, Card } from 'semantic-ui-react'; 
+import { styled, alpha } from '@mui/material/styles';
+import "./NavBar.css"
+import Box from '@mui/material/Box';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import SortIcon from '@mui/icons-material/Sort';
+import IconButton from '@mui/material/IconButton';
+import Drawer from '@mui/material/Drawer';
+import "./NavBar.css";
+import FilterDisplay from "./filterDisplay";
+import Grid from '@mui/material/Grid';
 
 function Characters({allCharacters}){
-
-   const characterDisplay = allCharacters.map((character,index)=>{
+    const [open, setOpen] = useState(false);
+    const [search,setSearch] = useState(null)
+    const [tier, setTier] = useState('');
+    const [avail, setAvail] = useState('');
+    const [appeared,setAppeared] = useState('');
+  
+ 
+    const characterDisplay = allCharacters.filter((character) =>{
+        if (search === null){
+            return character
+        }else if (search === character){
+            return character
+        }
+    }).map((character,index)=>{
         return <CharacterCard key={index} character={character} />
    })
 
@@ -56,12 +79,50 @@ function Characters({allCharacters}){
         }
     })*/
     return (
-<<<<<<< HEAD
         <Container style={{paddingTop: "6em"}}>
-=======
-        <Container style={{paddingTop: "6em"}} >
->>>>>>> 266c0e9f6864f35f8ced31549d3fbecceb722221
-            <Card.Group itemsPerRow={5}>
+            <Grid item xs={6} style={{textAlign: "right",display:"flex"}}>
+            <Autocomplete spacing={2} sx={{ width: 300,pr:4, pl:5 }}
+                    freeSolo
+                    onChange={handleSubmit}
+                    options={allCharacters}
+                    getOptionLabel={(character) => character.name}
+                    
+                    renderOption={(props,character) => (
+                      <Box component="li" 
+                      sx={{ '& > img': { mr: 2, flexShrink: 0 } }} 
+                      {...props}
+                      >
+                      <img
+                        loading="lazy"
+                        width="20"
+                        src={character.images.icon}
+                        srcSet={character.images.icon}
+                        alt=""
+                      />
+                      {character.name}
+                    </Box>
+                    )}
+                    renderInput={(params) => (
+                    <CssTextField
+                      {...params}
+                      style={{border:{color:'black'} }}
+                      label="Search"
+                      sx={{ input: { color: 'black' } }}
+                      InputProps={{
+                      ...params.InputProps,
+                      type: 'search',
+                      }}
+                    />)}
+                  />
+                 
+                  <IconButton  onClick={() => setOpen(true)}>
+                    <SortIcon style={{fontSize:"1.9em",color:"black",pr:10}}/>
+                  </IconButton>
+                  <Drawer open={open} anchor={"right"} onClose={() => setOpen(false)}>
+                    {getList()}
+                  </Drawer>
+                  </Grid>
+            <Card.Group itemsPerRow={5} style={{paddingTop:"2.5em"}}>
                 {characterDisplay}
             </Card.Group>
         </Container>
---
+++ my-app/src/components/Filter.js
@@ -0,0 +1,10 @@
+import React,{useState,useEffect} from "react";
+
+
+function Filter({value}){
+    console.log(value)
+    return(
+        <div></div>
+    )
+}
+export default Filter;
